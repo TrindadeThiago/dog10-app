30a0944ecaacf04feb2e3e884fbf9133fb4d94a2
Thiago Trindade <thiago.trindadexd@gmail.com>
2019-08-06 22:24:08 -0300

    Criação da home page e inicio da finalização do pedido

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,24 @@ export class HomePage {
 
   constructor() {}
 
+  hotdogList:Array<Object> = [
+    {
+      "title": "Hot Dog Simples",
+      "description": "Pão, maionese, salsicha e mostarda",
+      "price": "R$ 3,50",
+      "image": "assets/hotdog.jpg",
+    },
+    {
+      "title": "Hot Dog Mex",
+      "description": "Pão, maionese, Doritos e pasta mexicana",
+      "price": "R$ 7,50",
+      "image": "assets/hotdog2.jpg",
+    },
+    {
+      "title": "Hot Dog Sanduiche",
+      "description": "Pão, maionese, salsicha, catchup, alface, cebola roxa e tomate",
+      "price": "R$ 5,50",
+      "image": "assets/hotdog3.png",
+    }
+  ]
 }
